0535bd7cab888a4a51449d469299826791ffb7da
Tristan San Juan <tristansj88@gmail.com>
2021-02-17 11:53:53 -0700

    02b part1

--- data-access-layer/02b-get-ingredients-by-recipe-id.sql
+++ data-access-layer/02b-get-ingredients-by-recipe-id.sql
@@ -39,3 +39,7 @@
 
 
 -- YOUR CODE HERE
+SELECT ingredients.amount, ingredients.food_stuff, units_of_measure.name
+FROM ingredients
+JOIN units_of_measure ON(ingredients.unit_of_measure_id = units_of_measure.id)
+WHERE ingredients.recipe_id = $1;
